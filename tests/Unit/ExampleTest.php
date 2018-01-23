f535ee2f44b80eebd6c51abc46c0cca0e6e5c422
kimura_yuto <halu.k1126+testci1@gmail.com>
2018-01-23 17:38:16 +0900

    no message

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(false);
+        $this->visit('/')
+        ->see('Laraveeeel!!');
     }
 }
